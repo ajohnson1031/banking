5e83062cafa8d40eba9b1bf351f0957ba2e33ba1
ajohnson1031 <aaron.johnson1031@gmail.com>
2024-05-11 03:51:11 -0400

    add explanatory text on signin/signup pages

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,6 +3,18 @@ import AuthForm from "@/components/AuthForm";
 const SignIn = () => {
   return (
     <section className="flex-center size-full max-sm:px-6">
+      <div className="fixed w-[45%] top-10 right-[2.5%] z-50 bg-white p-4 border border-gray-200  font-semibold text-14 text-gray-600">
+        <p>Don&apos;t feel like signing up?</p>
+        <div className="flex gap-3">
+          <p className="">Test Account Credentials:</p>
+          <p>
+            Email: <span className="text-blue-700">test@testmail.com</span>
+          </p>
+          <p>
+            Password:<span className="text-blue-700"> TestTest</span>
+          </p>
+        </div>
+      </div>
       <AuthForm type="sign-in" />
     </section>
   );
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -3,6 +3,17 @@ import AuthForm from "@/components/AuthForm";
 const SignUp = async () => {
   return (
     <section className="flex-center size-full max-sm:px-6">
+      <div className="fixed w-[45%] top-10 right-[2.5%] z-50 bg-white p-4 border border-gray-200  font-semibold text-14 text-gray-600">
+        <div className="flex gap-3">
+          <p className="">First Platypus Bank Credentials:</p>
+          <p className="text-green-700">User: user_good</p>
+          <p className="text-green-700">Password: pass_good</p>
+        </div>
+        <p>For Code Verification, just click through.</p>
+        <p>
+          For linked account, you must select either <span className="underline">checking</span> or <span className="underline">savings</span>.
+        </p>
+      </div>
       <AuthForm type="sign-up" />
     </section>
   );
